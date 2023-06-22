8d7e1aea5fe80639248b27d0db636a91af6ba19e
jlbuild <juliabuildbot@gmail.com>
2023-06-22 14:22:00 +0000

    GraphicsMagick_jll build 1.3.40+0

---
+++ src/GraphicsMagick_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule GraphicsMagick_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("GraphicsMagick")
+JLLWrappers.@generate_main_file("GraphicsMagick", UUID("aa65733b-c888-5e24-8545-c87abc10c960"))
+end  # module GraphicsMagick_jll
